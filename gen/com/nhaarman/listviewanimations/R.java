e953ef302b70b14cffb1add2772cb16ade7d7fdc
novice313 <bochi.chen@gmail.com>
2015-09-13 01:01:11 +0800

    merge 1040913

--- gen/com/nhaarman/listviewanimations/R.java
+++ gen/com/nhaarman/listviewanimations/R.java
@@ -8,11 +8,11 @@ package com.nhaarman.listviewanimations;
 
 public final class R {
 	public static final class dimen {
-		public static final int activity_horizontal_margin = 0x7f060000;
-		public static final int activity_vertical_margin = 0x7f060001;
+		public static final int activity_horizontal_margin = 0x7f0b0000;
+		public static final int activity_vertical_margin = 0x7f0b0001;
 	}
 	public static final class drawable {
-		public static final int ic_launcher = 0x7f020043;
+		public static final int ic_launcher = 0x7f020048;
 	}
 	public static final class id {
 	}
@@ -21,15 +21,15 @@ public final class R {
 		public static final int fragment_main = 0x7f030017;
 	}
 	public static final class menu {
-		public static final int main = 0x7f0c0001;
+		public static final int main = 0x7f0d0001;
 	}
 	public static final class string {
-		public static final int action_settings = 0x7f070002;
-		public static final int app_name = 0x7f070000;
-		public static final int hello_world = 0x7f070001;
+		public static final int action_settings = 0x7f07002e;
+		public static final int app_name = 0x7f07002c;
+		public static final int hello_world = 0x7f07002d;
 	}
 	public static final class style {
-		public static final int AppBaseTheme = 0x7f080000;
-		public static final int AppTheme = 0x7f080001;
+		public static final int AppBaseTheme = 0x7f060005;
+		public static final int AppTheme = 0x7f060006;
 	}
 }
